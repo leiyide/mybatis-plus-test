c5b6fc0d7017d462b719e5f0685abe408b555cc3
leiyide <32489023+leiyide@users.noreply.github.com>
2020-08-20 13:29:10 +0800

    Update TestDeadLock.java

--- src/main/java/com/leiyide/utils/TestDeadLock.java
+++ src/main/java/com/leiyide/utils/TestDeadLock.java
@@ -14,7 +14,7 @@ import ch.qos.logback.core.util.TimeUtil;
 /**
  * @Title:TestDeadLock
  * @Description:  测试死锁
- * @Company: 维坤智能科技 
+ * @Company: 智能科技 
  * @version v0.1
  * @author leiyide
  * @date 2020年6月10日 上午11:13:15
