0aebedcb0c06a5f2d70863fc67a4e9b2e9d1b1c5
leiyide <32489023+leiyide@users.noreply.github.com>
2020-08-20 13:27:31 +0800

    Update MutexLock.java

--- src/main/java/com/leiyide/utils/MutexLock.java
+++ src/main/java/com/leiyide/utils/MutexLock.java
@@ -6,7 +6,7 @@ package com.leiyide.utils;
 /**
  * @Title:MutexLock
  * @Description:  互斥锁
- * @Company: 维坤智能科技 
+ * @Company: 智能科技 
  * @version v0.1
  * @author leiyide
  * @date 2020年6月10日 上午10:40:51
